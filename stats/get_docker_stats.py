4be5d03840dc2b0753cff6b6ade2162cd64a2a39
shayan ghani <shayanghani1384@gmail.com>
2024-03-28 11:56:26 +0330

    added  basic error handling for stats

--- stats/get_docker_stats.py
+++ stats/get_docker_stats.py
@@ -1,20 +1,22 @@
-import docker
-import time
-import asyncio
-
 def calculate_cpu_percentage(stats):
+    cpu_percent = 0
+    
     cpu_delta = stats['cpu_stats']['cpu_usage']['total_usage'] - stats['precpu_stats']['cpu_usage']['total_usage']
     system_delta = stats['cpu_stats']['system_cpu_usage'] - stats['precpu_stats']['system_cpu_usage']
     number_cpus = stats['cpu_stats']['online_cpus'] 
-    cpu_percent = (cpu_delta / system_delta) * number_cpus * 100.0 
+    if cpu_delta is not None and system_delta is not None and number_cpus is not None:
+        cpu_percent = (cpu_delta / system_delta) * number_cpus * 100.0 
     
     return cpu_percent
 
 def calculate_memory_usage(stats):
+    memory_percent = 0
+    memory_usage_bytes = 0
+    
     memory_usage_bytes = stats['memory_stats']['usage']
     memory_limit = stats['memory_stats']['limit']
-
-    memory_percent = (memory_usage_bytes / memory_limit) * 100.0
+    if memory_usage_bytes is not None and memory_limit is not None:
+        memory_percent = (memory_usage_bytes / memory_limit) * 100.0
 
     return memory_percent, memory_usage_bytes
     
@@ -41,36 +43,13 @@ def calculate_network_io(stats):
 
     if "networks" in stats:
         networks = stats["networks"]
-
-        for network in networks.values():
-            network_rx_bytes += network["rx_bytes"]
-            network_tx_bytes += network["tx_bytes"]
+        if network is not None:
+            for network in networks.values():
+                network_rx_bytes += network["rx_bytes"]
+                network_tx_bytes += network["tx_bytes"]
 
     return network_rx_bytes, network_tx_bytes
 
 async def get_container_stats(container):
     stats = await container.stats(stream=False)
     return stats
-
-
-
-# def test_stats(stats):
-#     start_stat = time.time()    
-#     memory_stats = stats['memory_stats']
-#     cpu_stats = stats['cpu_stats']
-#     precpu_stats = stats['precpu_stats']
-
-
-#     res = {
-#         "memory_usage": memory_stats['usage'],
-#         "memory_limit": memory_stats['limit'],
-#         "total_cpu_usage": cpu_stats['cpu_usage']['total_usage'],
-#         "total_precpu_usage": precpu_stats['cpu_usage']['total_usage'],
-#         "system_cpu_usage": cpu_stats['system_cpu_usage'],
-#         "system_precpu_usage": precpu_stats['system_cpu_usage'],
-#         "cpu_percent_len": len(stats['cpu_stats']['cpu_usage']['percpu_usage'])
-#     }
-
-#     end_stat = time.time() - start_stat
-#     print("res {:10.4f}".format(end_stat), "\n")   
-#     return res
